2a3423603a2f2ba5f55df6647685f554cfdde710
rohitgpt720 <rohitgpt720@gmail.com>
2020-02-29 17:54:19 +0530

    add smooth transitions

--- index.js
+++ index.js
@@ -66,3 +66,13 @@ var typed_2 = new Typed('#typed_2',{
   backSpeed:50,
   loop:true
 })
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+  anchor.addEventListener('click', function(e){
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
